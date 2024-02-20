34aff6a9c5f860801fe539528110c03db4fb08f0
Sócrates Freitas <68630680+Socrates-Freitas@users.noreply.github.com>
2024-02-20 16:55:41 -0300

    refactor: insertProductOnCart() refatorado

--- back/src/controllers/CartController.ts
+++ back/src/controllers/CartController.ts
@@ -8,14 +8,12 @@ class CartController {
   async create(req: Request, res: Response) {
     try {
       const { userId } = req.params;
-      const { value, quantity } = req.body;
 
       const totalValue = 0;
 
       const createdCart = await prisma.cart.create({
         data: {
           totalValue: totalValue,
-          quantity: 0,
           user: {
             connect: {
               id: Number(userId),
@@ -54,7 +52,7 @@ class CartController {
           userId: Number(userId),
         },
         orderBy: {
-          insertDate: "desc",
+          creationDate: "desc",
         },
       });
 
@@ -67,33 +65,54 @@ class CartController {
   async insertProductOnCart(req: Request, res: Response) {
     try {
       const { cartId } = req.params;
-      const { bookId, price, quantity } = req.body;
+      const { bookId, ammount } = req.body;
 
-      const totalValue = price * quantity;
+      const cart = await prisma.cart.findUnique({
+        where: {
+          id: Number(cartId),
+        },
+      });
 
       const book = await prisma.book.findUnique({
         where: {
           id: bookId,
         },
       });
+      let newTotalValue;
+      if (book && cart) {
 
-      const updatedCart = await prisma.cart.update({
-        where: {
-          id: Number(cartId),
-        },
+        newTotalValue = cart.totalValue;
+        newTotalValue = newTotalValue.plus(book.price.times(ammount));
+
+      }else{
+        return res.status(401).json("{error:{ message:\"No such items\" }")
+      }
+
+    const updatedCart = await prisma.cart.update({
+        where:{
+          id:Number(cartId)
+        },data:{
+          totalValue:newTotalValue
+        }
+      })
+
+      const newBookOnCart = await prisma.booksOnCart.create({
         data: {
-          totalValue: totalValue,
-          quantity: quantity,
-          booksOnCart: {
+          ammonut: ammount,
+          cart: {
             connect: {
-              cartId: Number(cartId),
-              bookId: bookId,
+              id: Number(cartId),
+            },
+          },
+          book: {
+            connect: {
+              id: bookId,
             },
           },
         },
       });
 
-      return res.status(201).json(updatedCart);
+      return res.status(201).json(newBookOnCart);
     } catch (error: any) {
       return res.status(500).json({ error: error.message });
     }
