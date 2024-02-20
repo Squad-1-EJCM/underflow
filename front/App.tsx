ffb5cb11443038867043b5082da00c97c261c649
AlphaOrionis20 <angelomvieira20@gmail.com>
2024-02-20 10:24:04 -0300

    Categories carousel created

--- front/App.tsx
+++ front/App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/HomeHeader/HomeHeader";
 import CategoriesCarousel from "./src/components/CategoriesCarousel/CategoriesCarousel";
+import CardBook from "./src/components/CardBook/CardBook";
 
 export default function App() {
   return (
     <View>
       <Header />
       <CategoriesCarousel />
+      <CardBook/>
     </View>
   );
 }
--- front/src/components/CategoriesCarousel/CategoriesCarousel.tsx
+++ front/src/components/CategoriesCarousel/CategoriesCarousel.tsx
@@ -1,14 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { FlatList } from "react-native";
+import { Container, Title, ItemView } from "./styles";
+
+const data = [
+  {
+    id: "1",
+    title: "Terror",
+    marginLeft: '16px'
+  },
+  {
+    id: "2",
+    title: "Aventura",
+  },
+  {
+    id: "3",
+    title: "Fantasia",
+  },
+  {
+    id: "4",
+    title: "Contos",
+  },
+  {
+    id: "5",
+    title: "Romance",
+  },
+  {
+    id: "6",
+    title: "Poesia",
+  },
+  {
+    id: "7",
+    title: "Ficção científica",
+  },
+];
+
+type ItemProps = { title: string, marginLeft:string };
+
+const Item = ({ title,marginLeft }: ItemProps) => (
+  <ItemView marginLeft={marginLeft ? marginLeft : "0px"}>
+    <Title>{title}</Title>
+  </ItemView>
+);
+
 
 
 const CategoriesCarousel = () => {
   return (
-    <View>
-      
-    </View>
-  )
-}
+    <Container>
+      <FlatList
+        data={data}
+        renderItem={({item}) => <Item title={item.title} marginLeft={item.marginLeft!} />}
+        keyExtractor={item => item.id}
+        horizontal={true}
+      />
+    </Container>
+  );
+};
 
-export default CategoriesCarousel
-
+export default CategoriesCarousel;
