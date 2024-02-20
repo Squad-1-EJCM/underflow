a9367140127a6a1db22d004668705a541b7183ed
Sócrates Freitas <68630680+Socrates-Freitas@users.noreply.github.com>
2024-02-20 10:56:55 -0300

    refactor: cpf para string, cep adicionado

--- back/src/models/migrations/20240219231426_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `author` on the `Book` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Book" DROP COLUMN "author";
