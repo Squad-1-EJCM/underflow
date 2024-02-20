12c19811550639bf715f782bd2a778f34357c761
Sócrates Freitas <68630680+Socrates-Freitas@users.noreply.github.com>
2024-02-19 21:42:26 -0300

    feature:  função createBook criada

--- back/src/controllers/BookController.ts
+++ back/src/controllers/BookController.ts
@@ -1,21 +1,80 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
-class BookController{
-    async create(req:Request, res:Response){
-        try{
+const prisma = new PrismaClient();
 
-            const {userId} = req.params
+class BookController {
+  async create(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const defaultImgUrl = ""; // TODO Gerenciar Upload de imagens
 
-           
-            
+      const {
+        title,
+        price,
+        edition,
+        authors,
+        categories,
+        language,
+        condition,
+        format,
+        description,
+      } = req.body;
 
-        }catch(error:any){
-            return res.status(500).json({error:error.message})
-        }
+      const createdBook = await prisma.book.create({
+        data: {
+          title: title,
+          price: price,
+          edition: edition,
+          language: language,
+          condition: condition,
+          format: format,
+          description: description,
+          userId: Number(userId),
+          imgUrl: defaultImgUrl,
+        },
+      });
+
+      await Promise.all(
+        authors.map((author: string) => {
+          prisma.author.create({
+            data: {
+              bookId: createdBook.id,
+              author: author,
+            },
+          });
+        })
+      );
+
+      await Promise.all(
+        categories.map((category: string) => {
+          prisma.category.create({
+            data: {
+              bookId: createdBook.id,
+              category: category,
+            },
+          });
+        })
+      );
+
+      return res.status(201).json(createdBook);
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
     }
+  }
+
+  async showAll(req:Request, res:Response){
+    try{
+        const AllBooks = await prisma.book.findMany()
+
+        return res.status(201).json(AllBooks)
+
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 
 
 }
 
-export default new BookController()
+export default new BookController();
