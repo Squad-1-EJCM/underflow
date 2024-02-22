74965819be435dbbd1d20b20c4997875278feb49
AlphaOrionis20 <angelomvieira20@gmail.com>
2024-02-22 19:48:26 -0300

    featured: A generic modal and a confirm modal were created

--- front/App.tsx
+++ front/App.tsx
@@ -4,10 +4,37 @@ import { View } from "react-native";
 import FinishPurchase from "./src/pages/FinishPurchase/FinishPurchase";
 import PurchaseButtons from "./src/components/PurchaseButtons/PurchaseButtons";
 import PurchaseCart from "./src/pages/PurchaseCart/PurchaseCart";
+import ModalConfirmation from "./src/components/Modal/Modal";
+import { ButtonTextCancel,
+  ButtonTextConfirm,
+  ButtonsView,
+  CancelButton,
+  ConfirmButton,
+  Icon,
+  IconVerifiedView,
+  ModalView,
+  OpenButton,
+  OpenButtonText,
+  Title,
+  TotalView, } from "./src/components/Modal/styles";
+import { useState } from "react";
+import Modal from "./src/components/Modal/Modal";
+import ConfirmModal from "./src/components/ConfirmModal/ConfirmModal";
+
+
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  
   return (
     //<Routes/>
-
-    <PurchaseCart />
+    <>
+    <OpenButton onPress={() => setIsModalVisible(true)}>
+        <OpenButtonText>ALO</OpenButtonText>
+      </OpenButton>
+<Modal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible}>
+<ConfirmModal setIsModalVisible={setIsModalVisible}/>
+</Modal>
+    </>
+  //<PurchaseCart />
   );
 }
